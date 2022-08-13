1b92cef6dc8f7bb13e583957b9159ab45a799b6f
bcvance <81263222+bcvance@users.noreply.github.com>
2022-08-12 19:52:06 -0500

    First Commit

---
+++ chat_app/consumers.py
@@ -0,0 +1,122 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import OnlineUser, TempUser
+from django.db.models.base import ObjectDoesNotExist
+import threading
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        self.user_id = self.scope['session']['online_user_id']
+        self.user_group_name = f'user_{self.user_id}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        await self.channel_layer.group_add(
+            self.user_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'connection_message',
+                'message': 'connected',
+                'message_username': self.scope['session']['username'],
+                'message_user_id': self.scope['session']['online_user_id'],
+            }
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message_username = text_data_json['message_username']
+
+        if 'type' in text_data_json:
+            if text_data_json['type'] == 'indiv_message':
+                message_user_id = text_data_json['message_user_id']
+                username = text_data_json['username']
+                await self.channel_layer.group_send(
+                    f'user_{message_user_id}',
+                    {
+                        'type': 'indiv_message',
+                        'message_username': message_username,
+                        'username': username,
+                    }
+            )
+        else:
+            message = text_data_json['message']
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message_username': message_username,
+                    'message': message,
+                }
+            )
+
+    async def chat_message(self, event):
+        message = event['message']
+        message_username = event['message_username']
+
+        await self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message,
+            'message_username': message_username,
+        }))
+
+    async def connection_message(self, event):
+        message = event['message']
+        message_username = event['message_username']
+        message_user_id = event['message_user_id']
+
+        await self.send(text_data=json.dumps({
+            'type': 'connection',
+            'message': message,
+            'message_username': message_username,
+            'message_user_id': message_user_id,
+        }))
+    
+    async def indiv_message(self, event):
+        message_username = event['message_username']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'type': 'indiv_message',
+            'message_username': message_username,
+            'username': username
+        }))    
+
+    async def disconnect(self, close_code):
+        self.thread = threading.Thread(target=self.delete_users)
+        self.thread.start()
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': 'disconnected',
+                'message_username': self.scope['session']['username']
+            }
+        )
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        del self.thread
+
+    def delete_users(self):
+        try:
+            print("try executed")
+            OnlineUser.objects.get(id = self.scope["session"]["online_user_id"]).delete()
+            TempUser.objects.get(id = self.scope["session"]["temp_user_id"]).delete()
+        except:
+            print("except executed")
+            pass
---
+++ chat_app/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from queue import Queue
+from .models import OnlineUser, TempUser, ChatRoom
+from django.db.models.base import ObjectDoesNotExist
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.core import serializers
+
+
+# Create your views here.
+def index(request):
+    num_online = OnlineUser.objects.all().count()
+    return render(request, "chat_app/index.html", context={
+        "num_online": num_online,
+    })
+
+def chat(request):
+    know_languages = request.POST["know-languages"]
+    learning_languages = request.POST["learning-languages"]
+    username = request.POST["username"]
+    request.session["username"] = username
+    try:
+        match = TempUser.objects.filter(knows=learning_languages, learning=know_languages).first()
+        print("found match")
+        room_name = match.room_name.id
+        room = match.room_name
+        online_user = OnlineUser.objects.create(username=username, room_name=room)
+        room.user2 = online_user.username
+        room.save()
+        request.session['online_user_id'] = online_user.id
+        request.session.modified = True
+        match.delete()
+    except (ObjectDoesNotExist, AttributeError) as e:
+        print("caught exception")
+        new_room = ChatRoom.objects.create()
+        online_user = OnlineUser.objects.create(username=username, room_name=new_room)
+        temp_user = TempUser.objects.create(username=username, knows=know_languages, learning=learning_languages, room_name=new_room)
+        new_room.user1 = online_user.username
+        new_room.save()
+        request.session["temp_user_id"] = temp_user.id
+        request.session['online_user_id'] = online_user.id
+        request.session.modifed = True
+        room_name = new_room.id
+    return render(request, "chat_app/chat.html", {
+        "know_lang": know_languages,
+        "learning_lang": learning_languages,
+        "room_name": room_name,
+        "username": username,
+    })
+
+# @api_view(['GET'])
+# def get_users(request, room_name):
+#     data = serializers.serialize('json', ChatRoom.objects.filter(id = room_name), fields=['user1', 'user2'])
+#     print(data)
+#     room = ChatRoom.objects.get(id=room_name)
+#     return Response(data)
